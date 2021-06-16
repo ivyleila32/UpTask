cb6f669f3ec5c73fc1069299851e15fefdd7ad5d
ivana di biase <delfi.17mdq@gmail.com>
2021-06-16 19:44:09 -0300

    añadiendo postgres sql

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const Sequelize = require ('sequelize');
 require('dotenv').config({path: 'variables.env' })
-const db = new Sequelize (process.env.DATABASE_URL);
+const db = new Sequelize (process.env.DATABASE_URL, {
+    ssl: true,
+});
 
 module.exports = db;
 
